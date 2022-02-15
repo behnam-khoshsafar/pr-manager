7f1322f18bc365682d89b4d17f644621c2d022c9
behnam-khoshsafar <b.khoshsafar@gmail.com>
2022-02-15 18:20:01 +0100

    -first commit -add authorization with github

---
+++ src/main/kotlin/com/bkh/prmanager/PrManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.bkh.prmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/bkh/prmanager/PrManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bkh.prmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
